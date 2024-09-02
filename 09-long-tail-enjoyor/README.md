f157fb3f7e07cd93bbfc00e720b03f695375751a
Carlos  Buendía <buendiacarlos@gmail.com>
2024-09-02 23:33:16 +0200

    improve 09-long-tail-enjoyor

--- 09-long-tail-enjoyor/README.md
+++ 09-long-tail-enjoyor/README.md
@@ -6,7 +6,24 @@ You’re an active member of the Synthetix community and noticed that the implem
     You can either:
      - mint sETH 1:1 with weth and sell it on the secondary markets when price is above peg, or
      - Buy it at a discount on secondary markets and sell it to this contract when it is trading below peg on secondary markets.
-- b) Provide the simulation data when you execute the opportunity using the full `maxETH` amount.
-    It can be found attached an example doing an arbitrage on block `12581549`, where actually used the mechanism to make a profit. 
+- b) Provide the simulation data when you execute the opportunity using the full `maxETH` amount. 
+    Assuming we created a bot we would have profited 8.3 ETH, as an opportunity was available for 306 blocks, since block `12427801` to block `12428106`, when [a bot finally took the full capacity of the wrapper](https://etherscan.io/tx/0x81f1dce97821f86198333eb67577cb2d931e49092dbb0f67e3c391e1574e091)
+
     
-    On this example, the capacity of the wrapper was only 2 ETH, so, even if we asked for `maxETH`, we would receive just 2 ETH. We will get back to the problem to try to find another bigger opportunity, where we can actually use the full `maxETH` (capacity full).
+    It can be found attached an example doing an arbitrage on block `12428106`, the same day the wrapper was deployed where it was actually possible to use `maxEth` to arb the pool, making a *profit of > 8.3 ETH*. 
+
+
+    The steps that we made in our test were the following
+
+    1. Get acceess to a large amount of ETH. We could have made it by doing a flashloan on dydx or aave [like this bot](https://etherscan.io/tx/0x81f1dce97821f86198333eb67577cb2d931e49092dbb0f67e3c391e1574e091 but to simplify the PoC we will just `mint` ourselves 5000 ETH
+     
+    2. Call the Synthetix wrapper to mint the max amount of ETH `synthetixWrapper.mint(maxEth);`
+
+    3. In exchange we receive `seth` 1:1
+
+    4. Sell `seth` on Curve pool, at a premium, receive 8.3044615 ETH profit
+
+    5. Repay loan
+
+
+![alt text](image.png)
--- 09-long-tail-enjoyor/simulations/test/SynthetixArb.t.sol
+++ 09-long-tail-enjoyor/simulations/test/SynthetixArb.t.sol
@@ -13,15 +13,11 @@ contract StaleAmm is Test {
     ICurvePool public curveEthSeth = ICurvePool(0xc5424B857f758E906013F3555Dad202e4bdB4567);
     ISynthetixEtherWrapper public synthetixWrapper = ISynthetixEtherWrapper(0xC1AAE9d18bBe386B102435a8632C8063d31e747C);
     address randomEOA = 0x95222290DD7278Aa3Ddd389Cc1E1d165CC4BAfe5; // Random EOA
-    address wethWhale = 0x2F0b23f53734252Bda2277357e97e1517d6B042A;
 
     uint256 deadline = 1800000000; // Far in the futures
 
 
     function setUp() public {
-        vm.deal(wethWhale, 1 ether);
-        vm.deal(randomEOA, 10001 ether); // 1000 to do the trade and 1 for gas to be kept in the account
-
         vm.startPrank(randomEOA);
 
         weth.approve(address(curveEthSeth), type(uint256).max);
@@ -29,62 +25,83 @@ contract StaleAmm is Test {
 
         weth.approve(address(synthetixWrapper), type(uint256).max);
         seth.approve(address(synthetixWrapper), type(uint256).max);
-
-        weth.deposit{value: 10000 ether}();
     }
     
     // In this arbitrage we will get advantage of a premium on SETH
     // We will swap 100 WETH for SETH using the Synthetix Wrapper and then swap SETH for WETH on Curve
     function testArbitragePremium () public {
 
-        uint256 maxMintableEth = synthetixWrapper.getReserves();
+        uint256 startingEthBalance = randomEOA.balance;
+        console2.log("Starting ETH balance: ", startingEthBalance);
+        
 
-        console2.log("Max mintable eth: ", maxMintableEth);
+        //////////////////////////////////////////////////////////////
+        // 1st we will simulate a flash loan of 10000 WETH          //
+        //////////////////////////////////////////////////////////////
+        uint256 maxEth  = synthetixWrapper.maxETH();
+        
+        uint256 loanAmount = maxEth;
+        vm.deal(randomEOA, loanAmount);
+ 
+        weth.deposit{value: loanAmount}();
 
-        // 1st we will swap WETH for SETH using the Synthetix Wrapper
+        
+        
+        //////////////////////////////////////////////////////////////
+        // 2nd we will swap WETH for SETH using the Synthetix Wrapper
+        //////////////////////////////////////////////////////////////
+
         uint256 wethBalanceBefore = weth.balanceOf(randomEOA);
         uint256 sethBalanceBefore = seth.balanceOf(randomEOA);
-        uint256 ethBalanceBefore = address(randomEOA).balance;
         uint256 wrapperCapacityBefore =  synthetixWrapper.capacity();
-        uint256 totalEthWethBefore = ethBalanceBefore + wethBalanceBefore;
 
-        console2.log("eth balance before: ", ethBalanceBefore);
         console2.log("weth balance before mint: ", wethBalanceBefore);
         console2.log("seth balance before mint: ", sethBalanceBefore);
         console2.log("capacity balance before mint: ", wrapperCapacityBefore);
-        console2.log("total eth + weth", totalEthWethBefore);
-        synthetixWrapper.mint(10000 ether);
+
+        synthetixWrapper.mint(loanAmount);
+
+
+        //////////////////////////////////////////////////////////////
+        //         3rd we will swap SETH for WETH using Curve      //
+        //////////////////////////////////////////////////////////////
 
         uint256 wethBalanceAfterMint = weth.balanceOf(randomEOA);
         uint256 sethBalanceAfterMint = seth.balanceOf(randomEOA);
-        uint256 ethBalanceAfterMint = address(randomEOA).balance;
         uint256 wrapperCapacityAfterMint =  synthetixWrapper.capacity();
-        uint256 totalEthWethAfterMint = ethBalanceAfterMint + wethBalanceAfterMint;
 
-        console2.log("eth balance after mint: ", ethBalanceAfterMint);
         console2.log("weth balance after mint / before curve exchange: ", wethBalanceAfterMint);
         console2.log("seth balance after mint / before curve exchane: ", sethBalanceAfterMint);
         console2.log("wrapper capacity after mint: ", wrapperCapacityAfterMint);
-        console2.log("total eth + weth after mint", totalEthWethAfterMint);
-        
-        curveEthSeth.exchange(1, 0, sethBalanceAfterMint , sethBalanceAfterMint);
 
+        curveEthSeth.exchange(1, 0, sethBalanceAfterMint , sethBalanceAfterMint);
 
         uint256 wethBalanceAfterExchange = weth.balanceOf(randomEOA);
         uint256 sethBalanceAfterExchange = seth.balanceOf(randomEOA);
-        uint256 ethBalanceAfterExchange = address(randomEOA).balance;
-        uint256 totalEthWethAfterExchange = ethBalanceAfterExchange + wethBalanceAfterExchange;
-
-        uint256 totalProfit = totalEthWethAfterExchange - totalEthWethBefore;
-
-
+        uint256 ethBalanceAfterExchange = randomEOA.balance;
+        
+        console2.log("weth balance after after curve exchange: ", wethBalanceAfterExchange);
+        console2.log("seth balance after curve exchane: ", sethBalanceAfterExchange);
+        console2.log("wrapper capacity after exchange: ", wrapperCapacityAfterMint);
         console2.log("eth balance after exchange: ", ethBalanceAfterExchange);
-        console2.log("weth balance after exchange: ", wethBalanceAfterExchange);
-        console2.log("seth balance after exchange: ", sethBalanceAfterExchange);
-        console2.log("total eth + weth after exchange", totalEthWethAfterExchange);
 
-        console2.log("Total profit: ", totalProfit); 
 
-        assertGt(totalProfit, 0, "Profit should be greater than 0");
+        //////////////////////////////////////////////////////////////
+        //         4th We repay the loan                           //
+        //////////////////////////////////////////////////////////////
+        
+
+        weth.withdraw(weth.balanceOf(randomEOA));
+        payable(address(0)).transfer(loanAmount);
+
+        uint256 ethBalanceAfterRepay = randomEOA.balance;
+        console2.log("eth balance after repay: ", ethBalanceAfterRepay);
+        console2.log("seth balance after repay: ", sethBalanceAfterExchange);
+
+        uint256 profit = ethBalanceAfterRepay - startingEthBalance;
+        
+        console2.log("Profit: ", profit);
+
+        //assertGt(profit, 0);
     }
 }
